ebf7feb9411a7c665286056cad29fe770e42a457
Mehedi Hassan <mehedi.thedue@gmail.com>
2019-01-11 00:45:19 +0600

    Create portfolio_resources.php

---
+++ resources/views/mahmud/portfolio_resources.php
@@ -0,0 +1,15 @@
+mixitup&magnific-popup.css ( though not going to use it)
+https://gist.github.com/hemusyl/6670439ae3d974429dccd05d78d90b48
+
+=================================================================================================
+lightbox after load more ajax
+https://stackoverflow.com/questions/14424389/lightbox-doesnt-work-on-new-content-generated-after-ajax-call
+=================================================================================================
+Better solution ( need to check it now)
+https://stackoverflow.com/questions/22312520/using-mixitup-plugin-in-image-gallery/22773954#22773954
+=================================================================================================
+Lightbox
+https://www.lokeshdhakar.com/projects/lightbox2/
+=================================================================================================
+
+=================================================================================================
